a0b0a9e7813bd58f1ead77e6c8e0888aa1f88e94
aydinschwa <aydin@recall.ai>
2026-01-23 01:34:14 -0800

    less bad terraform

--- terraform/server.tf
+++ terraform/server.tf
@@ -6,13 +6,6 @@ resource "digitalocean_droplet" "origin_server" {
     ssh_keys = [
         data.digitalocean_ssh_key.toy_cdn.id
     ]
-
-    connection {
-      host = self.ipv4_address
-      user = "root"
-      type = "ssh"
-      agent = true
-    }
 }
 
 variable "edge_server_regions" {
@@ -29,13 +22,6 @@ resource "digitalocean_droplet" "edge_servers" {
     ssh_keys = [
         data.digitalocean_ssh_key.toy_cdn.id
     ]
-
-    connection {
-      host = self.ipv4_address
-      user = "root"
-      type = "ssh"
-      agent = true
-    }
 }
 
 resource "digitalocean_droplet" "nameserver" {
@@ -47,26 +33,10 @@ resource "digitalocean_droplet" "nameserver" {
         data.digitalocean_ssh_key.toy_cdn.id
     ]
 
-    connection {
-      host = self.ipv4_address
-      user = "root"
-      type = "ssh"
-      agent = true
-    }
-
-    provisioner "file" {
-        content = jsonencode({
+    user_data = templatefile("${path.module}/cloud-init.yaml", {
+        config_json_content = jsonencode({
             origin_ip = digitalocean_droplet.origin_server.ipv4_address
             edge_server_ips = [for v in digitalocean_droplet.edge_servers: v.ipv4_address]
         })
-        destination = "/opt/config.json"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "cloud-init status --wait || true", # apparently I need this to keep terraform from killing itself
-            "apt-get update && apt-get install -y docker.io",
-            "docker run -d -p 53:53/udp -v /opt/config.json:/app/data/config.json aydinschwa/nameserver:latest"
-        ]
-    }
+    })
 }
