640bad28ed24e4f072c03a79ad2e0a2c0e1b1098
aydinschwa <aydin@recall.ai>
2026-01-22 23:12:56 -0800

    prep nameserver for production

--- src/server.py
+++ src/server.py
@@ -1,21 +1,19 @@
 import geoip2.database
+import json
 import math
 import socket
 from haversine import haversine # type: ignore
 from lib import DnsQueryPacket, build_dns_response, build_refused_response
 from typing import NamedTuple, Tuple
 
-HOST = "127.0.0.1"
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST = "0.0.0.0"
+PORT = 53  # standard DNS port
 
 
-FAKE_CLIENT_IP = "44.184.98.219"
-FAKE_EDGE_SERVER_IPS = [
-    "3.204.143.48",
-    "28.170.247.194",
-    "32.197.80.200",
-    "5.20.98.162"
-    ]
+with open("config.json", "r") as f:
+    config = json.load(f)
+    EDGE_SERVER_IPS = config["edge_server_ips"] 
+    ORIGIN_IP = config["origin_ip"]
 
 class EdgeServer(NamedTuple):
     ip: str
@@ -43,7 +41,7 @@ if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     sock.bind((HOST, PORT))
     reader = geoip2.database.Reader('../data/GeoLite2-City.mmdb')
-    edge_servers = [EdgeServer(ip, get_ip_coords(ip, reader)) for ip in FAKE_EDGE_SERVER_IPS]
+    edge_servers = [EdgeServer(ip, get_ip_coords(ip, reader)) for ip in EDGE_SERVER_IPS]
 
     while True:
         data, (client_ip, client_port) = sock.recvfrom(512)
@@ -52,21 +50,27 @@ if __name__ == "__main__":
         except Exception: # don't respond if we receive malformed or non-DNS traffic
             continue
 
-        # Refuse any queries other than those for an A record
-        if query_packet.question.record_type != 1:
+        # refuse any queries other than those for an A record
+        # refuse any queries for a random domain I don't own
+        if (query_packet.question.record_type != 1) or \
+           (not query_packet.question.domain_name.endswith("cdn-test.space")):
             response_packet = build_refused_response(query_packet) 
             sock.sendto(response_packet, (client_ip, client_port))
             continue
 
-        try:
-            client_coords = get_ip_coords(FAKE_CLIENT_IP, reader)
-            closest_server_ip = find_closest_server(client_coords, edge_servers)
-        except Exception as e:
-            print(f"Error while finding closest server for client {client_ip}: {e}")
-            closest_server_ip = edge_servers[0].ip
+        # return the origin server's IP if someone is requesting it directly
+        if query_packet.question.domain_name == "origin.cdn-test.space":
+            response_packet = build_dns_response(query_packet, ORIGIN_IP, 50)
 
-        response_packet = build_dns_response(query_packet, closest_server_ip, 50)
-        print(response_packet)
+        else:
+            try:
+                client_coords = get_ip_coords(client_ip, reader)
+                closest_server_ip = find_closest_server(client_coords, edge_servers)
+            except Exception as e:
+                print(f"Error while finding closest server for client {client_ip}: {e}")
+                closest_server_ip = edge_servers[0].ip
+            response_packet = build_dns_response(query_packet, closest_server_ip, 50)
+
         sock.sendto(response_packet, (client_ip, client_port))
 
 
