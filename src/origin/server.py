0d4889e74ea89f11103640d4ce3a2444d0900236
aydinschwa <aydin@recall.ai>
2026-01-30 15:20:09 -0800

    local deploy working

--- src/origin/server.py
+++ src/origin/server.py
@@ -1,19 +1,18 @@
+import logging
 import time
 import http.server
 import socketserver
 
-PORT = 8000
+logger = logging.getLogger(__name__)
+
+PORT = 80
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
-
-    def end_headers(self) -> None:
-        self.send_header("Cache-Control", "max-age=600, public")
-        return super().end_headers()
 
     def do_GET(self) -> None:
         time.sleep(1)
         return super().do_GET()
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print(f"Serving at port {PORT}")
+    logger.info(f"Serving at port {PORT}")
     httpd.serve_forever()
