215028406bd3fd1820d6fad6a4affc45f6d304dc
aydinschwa <aydin@recall.ai>
2026-02-01 22:45:20 -0800

    remove unnecessary sleep, latency is enough in the real world

--- src/origin/server.py
+++ src/origin/server.py
@@ -1,5 +1,4 @@
 import logging
-import time
 import http.server
 import socketserver
 
@@ -7,12 +6,6 @@ logger = logging.getLogger(__name__)
 
 PORT = 80
 
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-
-    def do_GET(self) -> None:
-        time.sleep(1)
-        return super().do_GET()
-
-with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
     logger.info(f"Serving at port {PORT}")
     httpd.serve_forever()
